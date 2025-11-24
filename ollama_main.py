d50f53e705ce6a4d0a54563ae0487ebafec4cf8f
virtualramblas <guglielmo.iozzia@gmail.com>
2025-11-24 19:47:23 +0000

    Reading input data from file.

--- ollama_main.py
+++ ollama_main.py
@@ -8,27 +8,34 @@ from prompts import (get_dataset_generation_system_prompt,
 from schema_generator import generate_schema_ollama
 from value_identification import extract_symbolic_triplets, generate_schema_aligned_triplets_ollama
 
-def main(input_text: str, model_id: str):
+def main(input_file_path: str, model_id: str, input_text: str):
     client = OllamaChatClient(
         model=model_id,
         system_prompt=get_dataset_generation_system_prompt()
     )
 
-    ### Generate mock text data ###
-    generated_mock_text = generate_mock_data_ollama(client, input_text)
-    print(generated_mock_text)
-    generated_mock_text_list = [generated_mock_text]
+    
+    if input_file_path != '':
+        ### Read text from file ###
+        input_file = open(input_file_path)
+        input_content = input_file.readlines()
+    else:
+        if input_text != '':
+            ### Generate mock text data ###
+            generated_mock_text = generate_mock_data_ollama(client, input_text)
+            print(generated_mock_text)
+            input_content = [generated_mock_text]
 
     ### Generate database schema ###
     client.reset()
     client.add_assistant_message(get_schema_generation_system_prompt_cot())
-    generated_schema = generate_schema_ollama(client, generated_mock_text)
+    generated_schema = generate_schema_ollama(client, input_content)
     print(generated_schema)
 
 
     ### Generate symbolic triplets ###
     symbolic_triplet_list = []
-    for paragraph in generated_mock_text_list:
+    for paragraph in input_content:
         symbolic_triplets = extract_symbolic_triplets(paragraph)
         for triplet in symbolic_triplets:
             print(triplet)
@@ -38,15 +45,16 @@ def main(input_text: str, model_id: str):
     client.reset()
     client.add_assistant_message(get_triplet_generation_system_prompt())
     schema_aligned_triplet_list = []
-    for paragraph in generated_mock_text_list:
+    for paragraph in input_content:
         schema_aligned_triplets = generate_schema_aligned_triplets_ollama(client, generated_schema, paragraph)
         print(schema_aligned_triplets)
         schema_aligned_triplet_list.append(schema_aligned_triplets)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Synthesis of relational databases from unstructured text")
-    parser.add_argument("--input_text", type=str, help="The text where to start generation.")
+    parser.add_argument("--input_file", type=str, default='', help="The path of the input document.")
     parser.add_argument("--model", type=str, default="qwen2.5:3b", help="The Ollama ID of the model to use.")
+    parser.add_argument("--input_text", type=str, default='', help="The text where to start generation (when no input file is provided).")
     args = parser.parse_args()
 
-    main(args.input_text, args.model)
+    main(args.input_file, args.model, args.input_text)
